5a0d4ef2d2294f9efbed8f8634d6037b081deac2
Marekvoelcke <maarelp@gmail.com>
2023-05-10 18:14:50 +0200

    bhw_1

---
+++ android/app/src/main/kotlin/com/example/bar_hop_alpha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bar_hop_alpha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'bar_profiles_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bar Hop Alpha',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+      routes: {
+        '/barProfiles': (context) => BarProfilesScreen(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Bar Profiles'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/barProfiles');
+          },
+        ),
+      ),
+    );
+  }
+}
